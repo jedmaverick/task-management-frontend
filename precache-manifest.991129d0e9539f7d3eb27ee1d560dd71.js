4b8e2eaaca78650224cbccc280b6710fc5bb2e54
jedmaverick <jedmaverick@users.noreply.github.com>
2021-06-06 14:20:52 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.991129d0e9539f7d3eb27ee1d560dd71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7f06a8829cbdb62b8ccc05626988bf",
+    "url": "https://jedmaverick.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "29c907b90b5cee59c179",
+    "url": "https://jedmaverick.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "4e43a2938a10ec7a46e7",
+    "url": "https://jedmaverick.github.io/task-management-frontend/static/js/2.f5c4a73d.chunk.js"
+  },
+  {
+    "revision": "29c907b90b5cee59c179",
+    "url": "https://jedmaverick.github.io/task-management-frontend/static/js/main.b1b5ffa5.chunk.js"
+  },
+  {
+    "revision": "3446756bdb07fa1993db",
+    "url": "https://jedmaverick.github.io/task-management-frontend/static/js/runtime~main.a28eabbe.js"
+  }
+]);
